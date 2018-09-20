df2d80029c74cf1881ebba33a5acf42f3af83441
DayanOA <dayan_olaya@yahoo.es>
2018-09-20 18:40:14 -0500

    Paquete Resource modificacion, pendiente ajustes en el paquete DTO

--- controlCalificaciones/src/main/java/edu/konrad/controlcalificaciones/dto/AreaDto.java
+++ controlCalificaciones/src/main/java/edu/konrad/controlcalificaciones/dto/AreaDto.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package edu.konrad.controlcalificaciones.dto;
 
 import edu.konrad.controlcalificaciones.entities.AreaEntity;
@@ -15,6 +11,10 @@ import java.util.List;
  * @author bwto
  */
 public class AreaDto {
+
+    public static List<AreaDto> toAreaList(List<AreaEntity> areas) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
     private long idArea;
     private String nombreArea;

980d05eec9e22e44eb25310e9db6beb8b9f64f9a
Roberto Garcia <40811272+bweto@users.noreply.github.com>
2018-10-06 00:12:37 -0500

    Update EstadoResource.java

--- controlCalificaciones/src/main/java/edu/konrad/controlcalificaciones/resource/EstadoResource.java
+++ controlCalificaciones/src/main/java/edu/konrad/controlcalificaciones/resource/EstadoResource.java
@@ -49,7 +49,7 @@ public class EstadoResource {
     */
     @POST
     public EstadoDto crearEstado(EstadoDto estadoNuevo){
-       return  new EstadoDto.crearEstado(estadoNuevo.toEntity());
+       return  new EstadoDto(estadoNuevo.toEntity());
         
     }
     

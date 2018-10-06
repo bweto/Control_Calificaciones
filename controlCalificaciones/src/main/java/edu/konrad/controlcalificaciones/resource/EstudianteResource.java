bc810a21eb935d4813807fe664c41e705b010a14
Roberto Garcia <40811272+bweto@users.noreply.github.com>
2018-10-06 00:04:28 -0500

    Update EstudianteResource.java

--- controlCalificaciones/src/main/java/edu/konrad/controlcalificaciones/resource/EstudianteResource.java
+++ controlCalificaciones/src/main/java/edu/konrad/controlcalificaciones/resource/EstudianteResource.java
@@ -48,7 +48,7 @@ public class EstudianteResource {
     */
     @POST
     public EstudianteDto crearEstudiante(EstudianteDto estudianteNuevo){
-        return EstudianteDto.crearEstudiante(estudianteNuevo.toEntity());
+        return new EstudianteDto.crearEstudiante(estudianteNuevo.toEntity());
         
     }
     
